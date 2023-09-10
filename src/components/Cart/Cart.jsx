08f3b6bebea2310a1e0855832179387939bf192a
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-09-10 18:36:02 +0600

    shop cart added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Cart.css';
 
+
 const Cart = ({cart}) => {
+
+    let total =0;
+    for(const product of cart){
+        total = total + product.price
+    }
     // const cart = props.cart; //option-1
     // const {cart} = props; // option-2
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: </p>
+            <p>Total Price: {total}</p>
             <p>Total Shipping: </p>
             <p>Tax</p>
             <h6>Grand Total</h6>
