e5bbc5f8822e265e85da6b7c2116a1d43651486f
mehadiHasanDiner <mehadi.cosmo@gmail.com>
2023-09-11 23:41:24 +0600

    cart component ready

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,20 +4,27 @@ import './Cart.css';
 
 const Cart = ({cart}) => {
 
-    let total =0;
+    let totalPrice =0;
+    let totalShippings =0;
     for(const product of cart){
-        total = total + product.price
+        totalPrice = totalPrice + product.price
+        totalShippings=totalShippings + product.shipping
     }
+
+    const tax = totalPrice*7/100;
+    const grandTotal = totalPrice+totalShippings+tax;
+
+
     // const cart = props.cart; //option-1
     // const {cart} = props; // option-2
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Selected Items: {cart.length}</p>
-            <p>Total Price: {total}</p>
-            <p>Total Shipping: </p>
-            <p>Tax</p>
-            <h6>Grand Total</h6>
+            <p>Total Price: {totalPrice}</p>
+            <p>Total Shipping: {totalShippings} </p>
+            <p>Tax {tax}</p>
+            <h6>Grand Total {grandTotal}</h6>
         </div>
     );
 };
